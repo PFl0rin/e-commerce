1802da1fbd7730d74496d4dfd97d183b773bcb33
Florin <pricopiflorin@yahoo.com>
2022-12-22 15:00:58 +0200

    init commit

---
+++ src/main/java/ro/florinpricopi/ecommercetechweb/ECommerceTechApplication.java
@@ -0,0 +1,13 @@
+package ro.florinpricopi.ecommercetechweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/florinpricopi/ecommercetechweb/ECommerceTechApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.florinpricopi.ecommercetechweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
